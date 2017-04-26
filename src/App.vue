6fc055ddd68c05d1f4935b22d609ada2f7a01eb5
Quincy <jayever@163.com>
2017-04-26 12:57:32 +0800

    3

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div id="app">
+
+    <x-header :right-options="{showMore: true}" @on-click-more="showMenus = true">with more menu</x-header>
+
     <router-view></router-view>
+
+    <tabbar>
+      <tabbar-item selected link="/main">
+        <i slot="icon" class="fa fa-home"></i>
+        <span slot="label">主页</span>
+      </tabbar-item>
+      <tabbar-item show-dot link="/classmain">
+        <i slot="icon" class="fa fa-users"></i>
+        <span slot="label">班级</span>
+      </tabbar-item>
+      <tabbar-item badge="2" link="/component/demo">
+        <i slot="icon" class="fa fa-comments"></i>
+        <span slot="label">沟通</span>
+      </tabbar-item>
+      <tabbar-item link="/user">
+        <i slot="icon" class="fa fa-user"></i>
+        <span slot="label">我的</span>
+      </tabbar-item>
+    </tabbar>
+
   </div>
 </template>
 
 <script>
+import { Tabbar, TabbarItem, XHeader  } from 'vux'
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Tabbar, TabbarItem, XHeader 
+  },
 }
 </script>
 
@@ -15,8 +42,12 @@ export default {
 
 body {
   max-width: 600px;
+  height:100%;
   margin:0 auto;
-  position: relative;
+  // position: relative;
   background-color: #fbf9fe;
+}
+.weui-tabbar{
+  max-width: 600px;
 }
 </style>
