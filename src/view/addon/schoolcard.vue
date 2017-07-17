b54922e7680e96f614d3d47e0d6c04a036ec430b
nullcss <nullyl@163.com>
2017-07-17 10:06:42 +0800

    css

--- src/view/addon/schoolcard.vue
+++ src/view/addon/schoolcard.vue
@@ -1,64 +1,68 @@
 <template>
   <div class="hello">
+    <has-no-student v-if="$store.state.hasNoStudent">
+    </has-no-student>
+    <div v-else>
+      <group>
+        <cell title="一卡通当前余额：" :value="data.Blance+'元'">
+        </cell>
+      </group>
   
-    <group>
-      <cell title="一卡通当前余额：" :value="data.Blance+'元'">
-      </cell>
-    </group>
-
-    <div class="noMsg" v-if="data.Log===0">当前没有消费记录</div>
-    
-    <group title="消费记录：">
-      <cell :title="i.Title" :inline-desc="i.CreateTime" v-for="(i,index) in data.Log" :key="index">
-        <div slot="value">
-          <span style="color: red">{{i.OpeaType}} {{i.Money}}</span>
-        </div>
-      </cell>
-    </group> 
-
-    <div class="btn">
-      <x-button type="primary" @click.native="loadMore">点击加载更多</x-button>
+      <div class="noMsg" v-if="data.Log===0">当前没有消费记录</div>
+  
+      <group title="消费记录：">
+        <cell :title="i.Title" :inline-desc="i.CreateTime" v-for="(i,index) in data.Log" :key="index">
+          <div slot="value">
+            <span style="color: red">{{i.OpeaType}} {{i.Money}}</span>
+          </div>
+        </cell>
+      </group>
+  
+      <div class="btn">
+        <x-button type="primary" @click.native="loadMore">点击加载更多</x-button>
+      </div>
     </div>
-
+  
   </div>
 </template>
 
 <script>
-import { Group,Cell,LoadMore,XButton } from 'vux'
+import { Group, Cell, LoadMore, XButton } from 'vux'
+import hasNoStudent from '@/components/hasNoStudent'
 
 export default {
   name: 'hello',
   components: {
-    Group,Cell,LoadMore,XButton
+    Group, Cell, LoadMore, XButton ,hasNoStudent
   },
   data() {
     return {
-      data:{
-        Blance:0,
-        Log:[],
+      data: {
+        Blance: 0,
+        Log: [],
       },
-      currentPage:1
+      currentPage: 1
     }
   },
-  methods:{
-    getData(){
+  methods: {
+    getData() {
       let para = {}
       para.currentPage = this.currentPage
       para.pagesize = 10
-      this.$API.getCardInfo(para).then(res=>{
-        this.data.Blance=res.Blance
-        res.Log.forEach((val)=>{
+      this.$API.getCardInfo(para).then(res => {
+        this.data.Blance = res.Blance
+        res.Log.forEach((val) => {
           this.data.Log.push(val)
         });
       })
     },
-    loadMore(){
+    loadMore() {
       this.currentPage++
       this.getData()
     }
   },
-  created(){
-    this.$store.commit('changeTitle','一卡通消费记录')
+  created() {
+    this.$store.commit('changeTitle', '一卡通消费记录')
     this.getData()
   },
   mounted() {
@@ -72,7 +76,8 @@ export default {
   text-align: center;
   line-height: 150px;
 }
-.btn{
-  padding:20px ;
+
+.btn {
+  padding: 20px;
 }
 </style>
--- src/view/student/score.vue
+++ src/view/student/score.vue
@@ -1,34 +1,37 @@
 <template>
   <div>
-    <div class="header card">
-      <p class="title">{{score.ExamName}}</p>
-      <p class="time">2017-5-10</p>
-    </div>
-
-    <div class="main card">
-      <div class="total">
-        <div class="item">
-          <span>名次</span>
-          <span class="num">20</span>
-        </div>
-        <div class="item">
-          <span>总分</span>
-          <span class="num">{{score.Score}}</span>
-        </div> 
+    <has-no-student v-if="$store.state.hasNoStudent">
+    </has-no-student>
+    <div v-else>
+      <div class="header card">
+        <p class="title">{{score.ExamName}}</p>
+        <p class="time">2017-5-10</p>
       </div>
-      <ul class="subject">
-        <li v-for="(list,index) in score.ScoreInfo" :key="index">
-          <span class="title">{{list.CourseName}}</span>
-          <span class="score">{{list.Score}}</span>
-          <span class="fullScore">/{{list.FullScore}}</span>
-        </li>
-      </ul>
-    </div>
-
-    <div class="more card" @click="showpopup=true,getExamList()">
-      查看历次成绩 >>>
-    </div>
-
+  
+      <div class="main card">
+        <div class="total">
+          <div class="item">
+            <span>名次</span>
+            <span class="num">20</span>
+          </div>
+          <div class="item">
+            <span>总分</span>
+            <span class="num">{{score.Score}}</span>
+          </div>
+        </div>
+        <ul class="subject">
+          <li v-for="(list,index) in score.ScoreInfo" :key="index">
+            <span class="title">{{list.CourseName}}</span>
+            <span class="score">{{list.Score}}</span>
+            <span class="fullScore">/{{list.FullScore}}</span>
+          </li>
+        </ul>
+      </div>
+  
+      <div class="more card" @click="showpopup=true,getExamList()">
+        查看历次成绩 >>>
+      </div>
+  
       <popup v-model="showpopup" height="100%" class="popup">
         <div class="close" @click="showpopup=false">返回</div>
         <div class="content">
@@ -40,55 +43,57 @@
             <div class="score">{{i.Score}}分</div>
           </li>
         </div>
-      </popup>   
-
+      </popup>
+    </div>
+  
   </div>
 </template>
 
 <script>
 import { Popup } from 'vux'
+import hasNoStudent from '@/components/hasNoStudent'
 
 export default {
   name: 'hello',
   components: {
-    Popup
+    Popup,hasNoStudent
   },
-  data () {
+  data() {
     return {
-      showpopup:false,
-      exam:[],
-      score:{}
+      showpopup: false,
+      exam: [],
+      score: {}
     }
   },
-  methods:{
-    getScoreData(){
-      this.$API.getExamScore(this.$route.params.studentId,this.$route.params.examId).then(res=>{
-        this.score=res
+  methods: {
+    getScoreData() {
+      this.$API.getExamScore(this.$route.params.studentId, this.$route.params.examId).then(res => {
+        this.score = res
       })
     },
-    getExamList(){
-      this.$API.getExamList(this.$route.params.studentId).then(res=>{
-        this.exam=res
+    getExamList() {
+      this.$API.getExamList(this.$route.params.studentId).then(res => {
+        this.exam = res
       })
     }
   },
-  created(){
-    this.$store.commit('changeTitle','成绩报告')
+  created() {
+    this.$store.commit('changeTitle', '成绩报告')
     this.getScoreData()
-    if(this.exam.length===0){
+    if (this.exam.length === 0) {
       this.$vux.toast.show({
         type: "text",
         text: '暂无考试信息',
-        width:'20em'
+        width: '20em'
       })
     }
   },
-  watch:{
-    showpopup(val){
-      if(val){
-        this.$store.commit('showNav',false)
-      }else{
-        this.$store.commit('showNav',true)
+  watch: {
+    showpopup(val) {
+      if (val) {
+        this.$store.commit('showNav', false)
+      } else {
+        this.$store.commit('showNav', true)
       }
     },
     "$route": "getScoreData"
@@ -97,120 +102,118 @@ export default {
 </script>
 
 <style lang="less" scoped>
-
-.header{
+.header {
   text-align: center;
   line-height: 1.6rem;
-  padding-bottom:14px;
-  .title{
-    padding:14px 14px 0 14px;
+  padding-bottom: 14px;
+  .title {
+    padding: 14px 14px 0 14px;
     font-size: 1rem;
   }
-  .time{
-    color:@cc3;
+  .time {
+    color: @cc3;
   }
 }
 
-.main{
+.main {
   text-align: center;
-  .total{
+  .total {
     line-height: 4rem;
-    padding:0 .5rem;
-    display:flex;
-    justify-content:center;
-    .item{
+    padding: 0 .5rem;
+    display: flex;
+    justify-content: center;
+    .item {
       font-size: 1rem;
-      width:50%;
+      width: 50%;
       border-bottom: 1px solid @cc4;
-      .num{
+      .num {
         font-weight: bold;
         font-size: 1.2rem;
-        color:@cc6;
+        color: @cc6;
       }
     }
   }
-  .subject{
-    display:flex;
+  .subject {
+    display: flex;
     flex-wrap: wrap;
-    padding:0 .5rem;
+    padding: 0 .5rem;
     position: relative;
     border-bottom: 1px solid @cc4;
-    &:before{
+    &:before {
       content: "";
       display: block;
       background: @cc4;
-      width:1px;
-      height:100%;
+      width: 1px;
+      height: 100%;
       position: absolute;
-      left:50%;
+      left: 50%;
     }
-    li{
-      width:50%;
+    li {
+      width: 50%;
       line-height: 3rem;
       border-bottom: 1px solid @cc4;
-      .score{
-        font-size:1.2rem;
+      .score {
+        font-size: 1.2rem;
         color: @cc6;
         font-weight: 600;
       }
-      .fullScore{
+      .fullScore {
         color: @cc2;
       }
-      &:last-child{
+      &:last-child {
         border-bottom: none;
       }
     }
   }
 }
 
-.more{
+.more {
   line-height: 2.5rem;
   text-align: center;
-  color:@cc2;
+  color: @cc2;
 }
 
-.popup{
+.popup {
   overflow: scroll;
-  .close{
-    background: rgba(0,0,0,0.4);
-    color:#fff;
+  .close {
+    background: rgba(0, 0, 0, 0.4);
+    color: #fff;
     line-height: 2rem;
     position: fixed;
-    width:100%;
-    padding-left:2rem;
+    width: 100%;
+    padding-left: 2rem;
   }
-  .content{
-    margin:2.5rem 0;
-    .examItem{
-      padding:.8rem 1rem;
+  .content {
+    margin: 2.5rem 0;
+    .examItem {
+      padding: .8rem 1rem;
       text-align: left;
       overflow: hidden;
-      .left{
+      .left {
         width: 80%;
         float: left;
         overflow: hidden;
       }
-      .title{
+      .title {
         font-size: 1rem;
         line-height: 1.5rem;
         height: 1.5rem;
         overflow: hidden;
       }
-      .time{
+      .time {
         display: inline-block;
-        color:@cc3;
+        color: @cc3;
         line-height: 1.5rem;
       }
-      .score{
+      .score {
         float: right;
         font-size: 1.1rem;
-        color:@cc6;
+        color: @cc6;
         width: 15%;
         text-align: right;
         margin-top: 10px;
       }
     }
   }
-
 }
 </style>
