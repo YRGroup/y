772ea8e6501b14d981b0ea7eefe8b05e20ceebbb
Quincy <jayever@163.com>
2017-08-16 12:32:22 +0800

    f

--- src/view/contact/allmsg.vue
+++ src/view/contact/allmsg.vue
@@ -1,49 +1,42 @@
 <template>
   <div class="msg">
-
-    <load-more :show-loading="false" tip="点击查看以前的消息" background-color="#f5f5f5"
-      @click.native="getMsgInfo()" v-if="data.length!=0"
-    ></load-more>
-
+  
+    <load-more :show-loading="false" tip="点击查看以前的消息" background-color="#f5f5f5" @click.native="getMsgInfo()" v-if="data.length!=0"></load-more>
+  
     <ul>
       <li class="item" :class="(item.SendTo===$route.params.userId)?'right':null" v-for="(item,index) in data" :key="index">
-        <div class="time"><span>{{item.CreateTime}}</span></div>
+        <div class="time">
+          <span>{{item.CreateTime}}</span>
+        </div>
         <img class="picinfo" :src="userImg">
         <span class="content">{{item.Content}}</span>
       </li>
-
+  
       <div class="nomsg" v-if="data.length===0">还没有消息</div>
     </ul>
-
+  
     <div class="replybtn">
-      <x-input class="msgcontent" type="text" 
-        :show-clear="false" 
-        placeholder="请在此输入要发送的内容" 
-        v-model="msg"
-        @keyup.enter.native="replyMsg"
-        ></x-input>
+      <x-input class="msgcontent" type="text" :show-clear="false" placeholder="请在此输入要发送的内容" v-model="msg" @keyup.enter.native="replyMsg"></x-input>
       <x-button class="msgbtn" type="primary" @click.native="replyMsg">发送</x-button>
     </div>
-
-
-
-    </br></br>
-    
-
+  
+    </br>
+    </br>
+  
   </div>
 </template>
 
 <script>
-import { XInput , XButton,LoadMore  } from 'vux'
+import { XInput, XButton, LoadMore } from 'vux'
 export default {
   name: 'hello',
   components: {
-    XInput , XButton,LoadMore 
+    XInput, XButton, LoadMore
   },
   data() {
     return {
-      showpopup:false,
-      msg:'',
+      showpopup: false,
+      msg: '',
       data: []
     }
   },
@@ -61,26 +54,26 @@ export default {
         this.userImg = res.sendto_Headimgurl
       })
     },
-    replyMsg(){
-      let msgdata={}
+    replyMsg() {
+      let msgdata = {}
       msgdata.sendto = this.$route.params.userId
       msgdata.content = this.msg
-      if(this.msg!=''){
-        this.$API.replyMsg(msgdata).then(res=>{
+      if (this.msg != '') {
+        this.$API.replyMsg(msgdata).then(res => {
           // this.$vux.toast.show({
           //   type:"text",
           //   text: '发送成功',
           //   width:'20em'
           // })
-          this.msg=''
+          this.msg = ''
           this.getMsgInfo()
-          window.scrollTo(0,10000)              
+          window.scrollTo(0, 10000)
         })
-      }else{
+      } else {
         this.$vux.toast.show({
-          type:"text",
+          type: "text",
           text: '消息不能为空',
-          width:'20em'
+          width: '20em'
         })
       }
     }
@@ -88,7 +81,7 @@ export default {
   created() {
     this.$store.commit('changeTitle', '消息中心')
     this.getMsgInfo()
-    window.scrollTo(0,10000)                  
+    window.scrollTo(0, 10000)
   },
   mounted() {
   }
@@ -96,37 +89,38 @@ export default {
 </script>
 
 <style lang="less" scoped>
-.msg{
-  padding-top:1px;
+.msg {
+  padding-top: 1px;
 }
-.replybtn{
+
+.replybtn {
   position: fixed;
   bottom: 4em;
-  left:0;
-  right:0;
-  padding:.5rem;
-  .msgcontent{
+  left: 0;
+  right: 0;
+  padding: .5rem;
+  .msgcontent {
     background: @cc3;
     display: inline-block;
-    width:calc(~"100% - 7.5rem");
+    width: calc(~"100% - 7.5rem");
     background: #fff;
   }
-  .msgbtn{
-    width:5rem;
+  .msgbtn {
+    width: 5rem;
     float: right;
   }
 }
-.nomsg{
+
+.nomsg {
   text-align: center;
-  padding:5rem;
-  // background: @cc4;
+  padding: 5rem; // background: @cc4;
 }
+
 .item {
   text-align: left;
   padding: 10px;
   font-size: 1.2em;
-  position: relative; 
-  // margin-bottom: 10px; 
+  position: relative; // margin-bottom: 10px; 
   .picinfo {
     position: absolute;
     left: 20px;
@@ -140,17 +134,19 @@ export default {
     margin: 0 10px 0 4rem;
     border-radius: 6px;
     z-index: -1;
-    word-break:break-all;
+    word-break: break-all;
     border-radius: 1px solid @cc4;
     box-shadow: 0 0 3px rgba(0, 0, 0, .1);
   }
 }
-.weui-loadmore{
-  margin:20px auto 0;
+
+.weui-loadmore {
+  margin: 20px auto 0;
 }
+
 .time {
   text-align: center;
-  color:@cc3;
+  color: @cc3;
   font-size: 0.8rem;
   margin-bottom: 8px;
 }
