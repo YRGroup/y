66fc0747953d902bc114412180ec70d233567b15
Quincy <jayever@163.com>
2017-09-14 09:27:46 +0800

    css fix

--- src/view/main/news.vue
+++ src/view/main/news.vue
@@ -1,23 +1,23 @@
 <template>
-  <div>
+  <div class="newsPage">
     <card>
-      <div slot="header" class="header newheader">
-        <div class="center newtitle">{{ data.Title }}</div>
-        <div class="center time">{{ data.AddTime }}</div>
+      <div slot="header" class="header">
+        <div class="newtitle">{{ data.Title }}</div>
+        <div class="time">{{ data.AddTime }}</div>
       </div>
-      <div slot="content" class="content newcontent">
+      <div slot="content" class="content">
         <pre v-html="data.content"></pre>
       </div>
     </card>
-  
+
     <div class="comment-header">
       <span>全部评论（{{data.Comments.length }}）</span>
       <span class="addbtn" @click="openreply">回复</span>
     </div>
-    <card class="comment cardcont" v-for="(i,index) in data.Comments" :key="index">
+    <card class="comment" v-for="(i,index) in data.Comments" :key="index">
       <div slot="header" class="header">
         <span class="headTextImg">{{i.UserName.substr(0,1)}}</span>
-         <!-- <img :src="i.Headimgurl" @click="fun('打开 '+i.UserName+' 的个人页面')">  -->
+        <!-- <img :src="i.Headimgurl" @click="fun('打开 '+i.UserName+' 的个人页面')">  -->
         <span class="usename">{{ i.UserName }}</span>
         <span class="time">{{ i.AddTime }}</span>
       </div>
@@ -25,7 +25,7 @@
         <p>{{i.Content }}</p>
       </div>
     </card>
-  
+
     <popup v-model="showpopup" height="180px" is-transparent>
       <div style="width: 95%;background-color:#fff;height:160px;margin:0 auto;border-radius:5px;padding-top:10px;">
         <group :title="'回复 '">
@@ -36,7 +36,7 @@
         </div>
       </div>
     </popup>
-  
+
   </div>
 </template>
 
@@ -88,9 +88,9 @@ export default {
       this.commentData.ArticleID = this.$route.query.id
       this.$API.addNewsComment(this.commentData).then(res => {
         this.$vux.toast.show({
-            type:"success",
-            text: "发布成功"
-          })
+          type: "success",
+          text: "发布成功"
+        })
         this.showpopup = false
         this.commentData.Content = ''
         this.getData()
@@ -107,78 +107,56 @@ export default {
 }
 </script>
 
-<style lang="less" scoped>
-.cardcont {
+<style lang="less">
+.newsPage {
   img {
-    width: 3.2em;
-    margin-right: 0.6em;
+    max-width: 100%;
   }
-  .usename {
-    font-size: 1.1em;
-    top: 1em;
-  }
-  .time {
-    top: 3em;
-  }
-  .reply {
-    position: absolute;
-    top: 2rem;
-    right: 0;
-    border: none;
-    color: @main;
-    padding: 0 1em;
-  }
-}
 
-.comment {
-  position: relative;
-  margin: 0;
-}
-
-.comment-header {
-  position: relative;
-  height: 2.6em;
-  line-height: 2.6em;
-  font-size: 1.2em;
-  background: #fff;
-  padding-left: 2em;
-  margin-top: 20px;
-  color: @grey;
-   :after {
-    content: "";
-    position: absolute;
-    width: 3px;
-    height: 1em;
-    top: 10px;
-    left: 1em;
-    background: @main;
+  .header {
+    margin-bottom: 20px;
+    .newtitle {
+      font-size: 1.2em;
+      text-align: center;
+    }
+    .time {
+      position: static;
+      text-align: center;
+    }
   }
-  .addbtn {
-    float: right;
-    padding: 0 1rem;
-    color: @main; // background: #03a9f4;
-    cursor: pointer; // border-top-left-radius: 15px;
-    // border-bottom-left-radius: 15px;
-  }
-}
-.center{
-  text-align: center;
-}
-.newheader{
-  margin-bottom: 20px;
-  .newtitle{
+  .comment-header {
+    position: relative;
+    height: 2.6em;
+    line-height: 2.6em;
     font-size: 1.2em;
+    background: #fff;
+    padding-left: 2em;
+    margin-top: 20px;
+    color: @grey;
+     :after {
+      content: "";
+      position: absolute;
+      width: 3px;
+      height: 1em;
+      top: 10px;
+      left: 1em;
+      background: @main;
+    }
+    .addbtn {
+      float: right;
+      padding: 0 1rem;
+      color: @main; // background: #03a9f4;
+      cursor: pointer; // border-top-left-radius: 15px;
+      // border-bottom-left-radius: 15px;
+    }
   }
-  .time{
-    position: static;
+  .comment {
+    position: relative;
+    margin: 0;
+    .vux-card-content {
+      padding-left: 120px;
+      margin-left: 120px;
+    }
   }
-}
-.newcontent{
-  overflow: hidden;
-  line-height: 1.4em;
-}
-.cardcont .vux-card-content{
-  padding-left: 120px;
-  margin-left: 120px;
 }
 </style>
