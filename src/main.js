d427f695198b86499c00e51dd931d549377871c8
Quincy <jayever@163.com>
2017-09-19 14:48:51 +0800

    delete h5-editor

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 import API from './server/API'
+import md5 from 'js-md5'
+import axios from 'axios'
+import {
+  LoadingPlugin,
+  ToastPlugin
+} from 'vux'
 
 FastClick.attach(document.body)
 
@@ -22,7 +28,7 @@ router.beforeEach((to, from, next) => {
 
 
 // 日期格式化
-Date.prototype.Format = function (fmt) { //author: meizz 
+Date.prototype.Format = function (fmt) {
   var o = {
     "M+": this.getMonth() + 1, //月份 
     "d+": this.getDate(), //日 
@@ -40,9 +46,6 @@ Date.prototype.Format = function (fmt) { //author: meizz
 
 Vue.prototype.$API = API
 
-import md5 from 'js-md5'
-
-import axios from 'axios'
 axios.defaults.withCredentials = true
 axios.interceptors.request.use(config => {
   let now = new Date().getTime()
@@ -85,121 +88,8 @@ axios.interceptors.response.use(
 )
 Vue.prototype.$http = axios
 
-import {
-  ToastPlugin
-} from 'vux'
 Vue.use(ToastPlugin)
-
-import {
-  LoadingPlugin
-} from 'vux'
 Vue.use(LoadingPlugin)
-
-import VueHtml5Editor from 'vue-html5-editor'
-let options = {
-  name: "vue-html5-editor",
-  showModuleName: false,
-  icons: {
-    text: "fa fa-pencil",
-    color: "fa fa-paint-brush",
-    font: "fa fa-font",
-    align: "fa fa-align-justify",
-    list: "fa fa-list",
-    link: "fa fa-chain",
-    unlink: "fa fa-chain-broken",
-    tabulation: "fa fa-table",
-    image: "fa fa-file-image-o",
-    hr: "fa fa-minus",
-    eraser: "fa fa-eraser",
-    undo: "fa-undo fa",
-    "full-screen": "fa fa-arrows-alt",
-    info: "fa fa-info",
-  },
-  image: {
-    sizeLimit: 512 * 1024,
-    upload: {
-      url: null,
-      headers: {},
-      params: {},
-      fieldName: {}
-    },
-    compress: {
-      width: 350,
-      height: 350,
-      quality: 80
-    },
-    uploadHandler(responseText) {
-      var json = JSON.parse(responseText)
-      if (!json.ok) {
-        alert(json.msg)
-      } else {
-        return json.data
-      }
-    }
-  },
-  language: "zh-cn",
-  i18n: {
-    "zh-cn": {
-      "align": "对齐方式",
-      "image": "图片",
-      "list": "列表",
-      "link": "链接",
-      "unlink": "去除链接",
-      "table": "表格",
-      "font": "文字",
-      "full screen": "全屏",
-      "text": "排版",
-      "eraser": "格式清除",
-      "info": "关于",
-      "color": "颜色",
-      "please enter a url": "请输入地址",
-      "create link": "创建链接",
-      "bold": "加粗",
-      "italic": "倾斜",
-      "underline": "下划线",
-      "strike through": "删除线",
-      "subscript": "上标",
-      "superscript": "下标",
-      "heading": "标题",
-      "font name": "字体",
-      "font size": "文字大小",
-      "left justify": "左对齐",
-      "center justify": "居中",
-      "right justify": "右对齐",
-      "ordered list": "有序列表",
-      "unordered list": "无序列表",
-      "fore color": "前景色",
-      "background color": "背景色",
-      "row count": "行数",
-      "column count": "列数",
-      "save": "确定",
-      "upload": "上传",
-      "progress": "进度",
-      "unknown": "未知",
-      "please wait": "请稍等",
-      "error": "错误",
-      "abort": "中断",
-      "reset": "重置"
-    }
-  },
-
-  hiddenModules: [],
-  visibleModules: [
-    "text",
-    "color",
-    "font",
-    "align",
-    "list",
-    "link",
-    "unlink",
-    "tabulation",
-    "image",
-    "hr",
-    "eraser",
-    "undo"
-  ],
-}
-Vue.use(VueHtml5Editor, options)
 
 new Vue({
   router,
