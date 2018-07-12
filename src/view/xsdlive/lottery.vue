296e5ed60eb8f8c2c76981847478edf5f29674f4
nullcss <nullyl@163.com>
2018-07-12 09:05:03 +0800

    xisida

--- src/view/xsdlive/lottery.vue
+++ src/view/xsdlive/lottery.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="wrapper">
-    <div class="item">
-      <ul class="userlist">
-        <li v-for="item in userlist" class="user">
-          <img :src="item.img" alt="">
-          <p>{{item.name}}</p>
-        </li>
-      </ul>
+    <div class="people">
+      <img :src="logo" alt="西斯达大树幼儿园">
     </div>
+    <div class="btn"></div>
   </div>
 </template>
 
@@ -18,47 +14,7 @@ export default {
   name: 'lottery',
   data() {
     return {
-      userlist:[
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦1"
-        },
-        {
-          img: require('@/assets/face/jobs.jpg'),
-          name: "周杰伦2"
-        },
-        {
-          img: require('@/assets/face/lh.jpg'),
-          name: "周杰伦3"
-        },
-        {
-          img: require('@/assets/face/tc.png'),
-          name: "周杰伦4"
-        },
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        },
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        },
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        },
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        },
-        {
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        },{
-          img: require('@/assets/face/jay.jpg'),
-          name: "周杰伦"
-        }
-      ]
+      logo:require('@/assets/xsdlogo.jpg')
     }
   },
 
@@ -72,32 +28,5 @@ export default {
 </script>
 
 <style lang="less" scoped> 
-.wrapper{
-  height: 100vh;
-  padding: 240px 200px 0;
-  box-sizing:border-box;
-  background:url(../../assets/xsdLiveBg.jpg) no-repeat center center;
-  background-size: cover; 
-  color: #fff;
-}
-.item{
-}
-.userlist{
-  display: flex;
-  flex-wrap: wrap;
-  flex-direction: row;
-  justify-content: flex-start;
-  .user{
-    text-align: center;
-    font-size: 16px;
-    width: 12.5%;
-    margin-bottom: 50px;
-    img{
-      width: 120px;
-      height: 120px;
-      border-radius: 50%;
-      box-shadow: 0 0 5px 5px rgba(0,0,0,.1);
-    }
-  }
-}
+
 </style>
--- src/view/xsdlive/signin.vue
+++ src/view/xsdlive/signin.vue
@@ -1,5 +1,14 @@
 <template>
-  
+  <div class="wrapper">
+    <div class="item">
+      <ul class="userlist">
+        <li v-for="item in userlist" class="user">
+          <img :src="item.img" alt="">
+          <p>{{item.name}}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -7,7 +16,48 @@ export default {
     return {
       liveId:0,
       curid:0,
-      count:2
+      count:2,
+      userlist:[
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦1"
+        },
+        {
+          img: require('@/assets/face/jobs.jpg'),
+          name: "周杰伦2"
+        },
+        {
+          img: require('@/assets/face/lh.jpg'),
+          name: "周杰伦3"
+        },
+        {
+          img: require('@/assets/face/tc.png'),
+          name: "周杰伦4"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },{
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        }
+      ]
     }
   },
   components:{
@@ -21,10 +71,10 @@ export default {
         count:this.count,
         getlottery:0
       }
-
     this.$API.getSignInList().then(res=>{
       
     })
+    }
   },
   created:{
     
@@ -32,4 +82,33 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+
+.wrapper{
+  height: 100vh;
+  padding: 240px 200px 0;
+  box-sizing:border-box;
+  background:url(../../assets/xsdLiveBg.jpg) no-repeat center center;
+  background-size: cover; 
+  color: #fff;
+}
+.item{
+}
+.userlist{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  .user{
+    text-align: center;
+    font-size: 16px;
+    width: 12.5%;
+    margin-bottom: 50px;
+    img{
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      box-shadow: 0 0 5px 5px rgba(0,0,0,.1);
+    }
+  }
+}
 </style>
