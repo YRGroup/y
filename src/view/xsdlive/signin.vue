206856dab2af20333a55efe9f9fc6afd904a6d45
nullcss <nullyl@163.com>
2018-07-12 11:29:08 +0800

    xsd

--- src/view/xsdlive/signin.vue
+++ src/view/xsdlive/signin.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="wrapper">
     <div class="item">
-      <transition-group tag="ul" class="userlist">
-          <li v-for="(item,index) in SignInList" class="user" :key="index">
+      <transition-group tag="ul" class="userlist list-complete">
+          <li v-for="(item,index) in SignInList" class="user list-complete-item" :key="index">
             <img :src="item.img" alt="">
             <p>{{item.name}}</p>
           </li>
@@ -14,113 +14,122 @@
 export default {
   data() {
     return {
-      liveId:0,
-      curid:0,
-      count:2,
+      liveId: 0,
+      curid: 0,
+      count: 2,
       SignInList: [],
-      userlist:[
+      userlist: [
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦1"
         },
         {
-          img: require('@/assets/face/jobs.jpg'),
+          img: require("@/assets/face/jobs.jpg"),
           name: "周杰伦2"
         },
         {
-          img: require('@/assets/face/lh.jpg'),
+          img: require("@/assets/face/lh.jpg"),
           name: "周杰伦3"
         },
         {
-          img: require('@/assets/face/tc.png'),
+          img: require("@/assets/face/tc.png"),
           name: "周杰伦4"
         },
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
         },
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
         },
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
         },
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
         },
         {
-          img: require('@/assets/face/jay.jpg'),
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
-        },{
-          img: require('@/assets/face/jay.jpg'),
+        },
+        {
+          img: require("@/assets/face/jay.jpg"),
           name: "周杰伦"
         }
       ]
-    }
+    };
   },
 
-  components: {
-
-  },
+  components: {},
   methods: {
     getSignInList() {
       let para = {
-        name:this.liveId,
-        curid:this.curid,
-        count:this.count,
-        getlottery:0
-      }
-    this.$API.getSignInList().then(res=>{
-      console.log(res)
-      this.SignInList = res.signInList.slice(0 ,24)
-
-    })
+        lid: this.liveId,
+        curid: this.curid,
+        count: this.count,
+        getlottery: 0
+      };
+      this.$API.getSignInList(para).then(res => {
+        this.SignInList = res.signInList.slice(0, 24);
+        console.log(res)
+      });
     }
   },
   created() {
     this.getSignInList();
+  },
+  watch: {
+    getNewUser() {
+      setTimeout(() => {
+        this.getSignInList()
+      },2000)
+    }
   }
 };
 </script>
 
 <style lang="less" scoped>
-
-.wrapper{
+.wrapper {
   height: 100vh;
   padding: 240px 200px 0;
-  box-sizing:border-box;
-  background:url(../../assets/xsdLiveBg.jpg) no-repeat center center;
-  background-size: cover; 
+  box-sizing: border-box;
+  background: url(../../assets/xsdLiveBg.jpg) no-repeat center center;
+  background-size: cover;
   color: #fff;
 }
-.item{
+.item {
 }
-.userlist{
+.userlist {
   display: flex;
   flex-wrap: wrap;
   flex-direction: row;
   justify-content: flex-start;
-  .user{
+  .user {
     text-align: center;
     font-size: 16px;
     width: 12.5%;
     margin-bottom: 50px;
-    img{
+    img {
       width: 120px;
       height: 120px;
       border-radius: 50%;
-      box-shadow: 0 0 5px 5px rgba(0,0,0,.1);
+      box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.1);
     }
   }
 }
-.component-fade-enter-active, .component-fade-leave-active {
-  transition: opacity .3s ease;
+.list-complete-item {
+  transition: all 1s;
+  display: inline-block;
 }
-.component-fade-enter, .component-fade-leave-to
-/* .component-fade-leave-active for below version 2.1.8 */ {
+.list-complete-enter,
+.list-complete-leave-to {
   opacity: 0;
+  transform: translateY(30px);
+}
+.list-complete-leave-active {
+  position: absolute;
 }
 </style>
