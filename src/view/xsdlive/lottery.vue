c4104cccbe3d626def33c04608dd8d953690f532
nullcss <nullyl@163.com>
2018-07-12 08:49:00 +0800

    xisida

--- src/view/xsdlive/lottery.vue
+++ src/view/xsdlive/lottery.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="wrapper">
-    
+    <div class="item">
+      <ul class="userlist">
+        <li v-for="item in userlist" class="user">
+          <img :src="item.img" alt="">
+          <p>{{item.name}}</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -8,9 +15,50 @@
 
 
 export default {
-  name: 'hello',
+  name: 'lottery',
   data() {
     return {
+      userlist:[
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦1"
+        },
+        {
+          img: require('@/assets/face/jobs.jpg'),
+          name: "周杰伦2"
+        },
+        {
+          img: require('@/assets/face/lh.jpg'),
+          name: "周杰伦3"
+        },
+        {
+          img: require('@/assets/face/tc.png'),
+          name: "周杰伦4"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },
+        {
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        },{
+          img: require('@/assets/face/jay.jpg'),
+          name: "周杰伦"
+        }
+      ]
     }
   },
 
@@ -25,6 +73,31 @@ export default {
 
 <style lang="less" scoped> 
 .wrapper{
-    background:url(../../assets/xsdLiveBg.jpg) no-repeat center center;
+  height: 100vh;
+  padding: 240px 200px 0;
+  box-sizing:border-box;
+  background:url(../../assets/xsdLiveBg.jpg) no-repeat center center;
+  background-size: cover; 
+  color: #fff;
+}
+.item{
+}
+.userlist{
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: row;
+  justify-content: flex-start;
+  .user{
+    text-align: center;
+    font-size: 16px;
+    width: 12.5%;
+    margin-bottom: 50px;
+    img{
+      width: 120px;
+      height: 120px;
+      border-radius: 50%;
+      box-shadow: 0 0 5px 5px rgba(0,0,0,.1);
+    }
+  }
 }
 </style>
